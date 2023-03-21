279f0724f6481ca5b09af14d5f5b548dfe3953c8
AsafWeitzman <asafweit1@gmail.com>
2023-03-21 13:09:54 +0200

    added router and pages components

---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,4 @@
+const ArticlePage = () => {
+  return <h1>This is the article page</h1>;
+};
+export default ArticlePage;
---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,4 @@
+const ArticlesListPage = () => {
+  return <h1>This is the articles list page</h1>;
+};
+export default ArticlesListPage;
